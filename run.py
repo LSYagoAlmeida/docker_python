d65253a570633c1d5149dca65af8752cfd9d1a7d
Yago Almeida <yago_almeida_luz@hotmail.com>
2024-02-14 18:29:03 -0300

    run py

--- run.py
+++ run.py
@@ -1,7 +1,20 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+import requests
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/cnpj/<cnpj>')
+def get_cnpj(cnpj):
+    url = f'https://receitaws.com.br/v1/cnpj/{cnpj}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return jsonify(data)
+    else:
+        return jsonify({'error': 'Erro ao consultar CNPJ'}), response.status_code
+
+if __name__ == '__main__':
+    app.run(debug=True)
